527ee6be12c57bde3fc3d1280810d13425338408
Itaru Kitagawa <itaruxkita@gmail.com>
2016-10-12 01:34:23 +0900

    Impl ScreenFill by assenbly

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,57 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  @KBD
+  D=M
+  @SET_BLACK
+  D;JNE
+  @SET_WHITE
+  0;JMP
+
+(SET_BLACK)
+  @color
+  M=-1
+  @SCREEN_FILL
+  0;JMP
+
+(SET_WHITE)
+  @color
+  M=0
+  @SCREEN_FILL
+  0;JMP
+
+(SCREEN_FILL)
+  // counter = 8191
+  @8192
+  D=A
+  @counter
+  M=D
+
+(SCREEN_LOOP)
+  // counter = counter - 1
+  @counter
+  M=M-1
+
+  // SCREEN[counter] = color
+  @SCREEN
+  D=A
+  @counter
+  D=D+M
+  @screenPos
+  M=D
+  @color
+  D=M
+  @screenPos
+  A=M
+  M=D
+
+  // if (i > 0) go back to SCREEN_WORD
+  @counter
+  D=M
+  @SCREEN_LOOP
+  D;JGT
+
+  @LOOP
+  0;JMP
