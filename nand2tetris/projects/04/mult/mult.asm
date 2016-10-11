66c32fef7ae25de30871bb96100d13940675bf15
Itaru Kitagawa <itaruxkita@gmail.com>
2016-10-11 23:28:43 +0900

    Impl Multiplier with assenbly

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// set sum = 0
+@sum
+M=0
+
+// set countdown = R1
+  @R1
+  D=M
+  @countDown
+  M=D
+
+(LOOP)
+  // if (countDown <= 0) go back to loop
+  @countDown
+  D=M
+  @RESULT
+  D;JLE
+
+  // sum = R0 + sum
+  @R0
+  D=M
+  @sum
+  M=D+M
+
+  // countDown = countDown - 1
+  @countDown
+  M=M-1
+  @LOOP
+  0;JMP
+
+// R2 = sum
+(RESULT)
+  @sum
+  D=M
+  @R2
+  M=D
+
+(END)
+  @END
+  0;JMP
